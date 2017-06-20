e23c0308dd1db3230721809daa2c238cdbce10e4
Brett Beaulieu-Jones <brettbj@gmail.com>
2017-06-19 23:52:19 -0400

    pre-publication prep

--- ScoreEpochs.sh
+++ ScoreEpochs.sh
@@ -1,10 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 10
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 11
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 12
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 13
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 14
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 15
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 16
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 17
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 18
-CUDA_VISIBLE_DEVICES=0 python score_epochs.py --id 19
+python score_epochs.py --id 8
+python score_epochs.py --id 9
